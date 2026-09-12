1d09f987737841432176d3fe449b53b759becb24
Blurryface2011 <contact@eau-potable.info>
2026-09-12 20:39:11 +0100

    Create backend.py

---
+++ backend.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Backend Discord Dashboard OK"
+
+@app.route("/health")
+def health():
+    return {"status": "ok"}
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)

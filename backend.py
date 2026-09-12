51808ca99b6260f5109e60925a7ce2dee87d2fda
Blurryface <contact@eau-potable.info>
2026-09-12 20:55:58 +0100

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
+import os
 
 app = Flask(__name__)
+
+# Autorise GitHub Pages à communiquer avec Render
+CORS(app)
 
 @app.route("/")
 def home():
@@ -11,6 +16,5 @@ def health():
     return {"status": "ok"}
 
 if __name__ == "__main__":
-    import os
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
